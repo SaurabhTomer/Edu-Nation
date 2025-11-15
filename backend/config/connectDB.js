215d24d1b38ecda089316d0f445131ac4b9a43e1
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-15 12:39:40 +0530

    user model

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("DB connected");
+        console.log("DataBase connected");
         
     } catch (error) {
         console.log("Mongo db connection error"); 
