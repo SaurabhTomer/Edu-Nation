3c70c66225b148b6814a40e076a3e4084f81c352
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-18 17:04:43 +0530

    middleware screate dof isAuth

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,10 +1,25 @@
-import jwt from 'jsonwebtoken'
-import { cookieParser } from 'cookie-parser';
+import jwt from "jsonwebtoken";
 
-export const isAuth = async (req,res) => {
-    try {
-        const {token } = req.cookieParser
-    } catch (error) {
-        
+
+export const isAuth = async (req, res) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(400).json({ message: "User doesn't have token" });
     }
-}
+    let verifyToken = await jwt.verify(token, process.env.JWT_SECRET);
+    if (!verifyToken) {
+      return res
+        .status(400)
+        .json({ message: "User doesn't have valid  token" });
+    }
+    //request .userid k andr verifryotken ki user id dal di
+    req.userId = verifyToken.userId
+    next()
+  } catch (error) {
+    return res
+        .status(500)
+        .json({ message: `IsAuth error ${error}` });
+    }
+  
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { login, logout, signup } from '../controllers/auth.controller.js'
+import { isAuth } from '../middlewares/auth.middleware.js'
 
 const authRouter = express.Router()
 
 //auth routes
-authRouter.post('/signup' , signup)
-authRouter.post('/login' , login)
-authRouter.get('/logout' , logout)
+authRouter.post('/signup' , isAuth,signup)
+authRouter.post('/login' ,isAuth, login)
+authRouter.get('/logout' ,isAuth, logout)
 
 export default authRouter
