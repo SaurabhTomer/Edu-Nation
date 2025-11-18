c0e1d4504acb0fd7033ab0a8ef6ca2fcda453e27
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-18 22:56:57 +0530

    started signup

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='bg-[#dddbdb] w-screen h-screen flex items-center justify-center'>
+        <form className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex'>
+            <div className='md:w-[50%] w-full h-full flex flex-col items-center justify-center gap-3'>
+                <h1>Let's get Started</h1>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Signup
