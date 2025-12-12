91678b466fd6b774d94f894428fea59e7f3c6fbc
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-12 22:27:15 +0530

    import images and home page correction

---
+++ frontend/src/components/Logos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logos() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Logos
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,35 @@
-import Nav from "../components/Nav"
-
-
+import Nav from "../components/Nav";
+import home from "../assets/home.jpg";
+import { SiViaplay } from "react-icons/si";
+import ai from "../assets/ai.png";
+import ai1 from "../assets/searchAi.png";
+import Logos from "../components/Logos";
 
 function Home() {
   return (
     <div className="w-full overflow-hidden">
       <div className="w-full lg:h-[140vh] h-[70vh] relative">
-        <Nav/>
+        <Nav />
+        <img src={home} alt="LMS background" />
+
+        <span>Grow Your Skills to Advance</span>
+        <span>Your Carrer Path</span>
+
+        <div>
+          <button>
+            View All Courses <SiViaplay />{" "}
+          </button>
+
+          <button>
+            Search with AI
+            <img src={ai} />
+            <img src={ai1} />{" "}
+          </button>
+        </div>
+        <Logos/>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
