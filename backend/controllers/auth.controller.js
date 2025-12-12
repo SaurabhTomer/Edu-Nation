4e0ace829ba78b2888800dca6731148639f47827
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-12 16:22:27 +0530

    controller of otp's

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,39 +1,41 @@
 import User from "../models/user.model.js";
 import validator from "validator";
 import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken'
-import { OAuth2Client } from 'google-auth-library';
+import jwt from "jsonwebtoken";
+import { OAuth2Client } from "google-auth-library";
+import { sendMail } from "./../config/sendMail.js";
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const client = new OAuth2Client(CLIENT_ID);
 
 //google auth
-export  const googleAuth = async(req, res) => {
+export const googleAuth = async (req, res) => {
   try {
     const { id_token, role } = req.body;
     if (!id_token) {
-      return res.status(400).json({ ok: false, message: 'id_token required' });
+      return res.status(400).json({ ok: false, message: "id_token required" });
     }
 
     // verify google id_token
     const ticket = await client.verifyIdToken({
-       idToken: id_token,
-        audience: process.env.GOOGLE_CLIENT_ID ,
-      });
+      idToken: id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
     const payload = ticket.getPayload();
 
     if (!payload) {
-      return res.status(401).json({ ok: false, message: 'Invalid token' });
+      return res.status(401).json({ ok: false, message: "Invalid token" });
     }
 
     // You may require verified email:
     if (!payload.email_verified) {
-      return res.status(403).json({ ok: false,
-         message: 'Google email not verified' });
+      return res
+        .status(403)
+        .json({ ok: false, message: "Google email not verified" });
     }
-//extract google profile data
+    //extract google profile data
     const email = payload.email;
-    const name = payload.name || '';
+    const name = payload.name || "";
     const googleId = payload.sub;
     const avatar = payload.picture;
 
@@ -41,7 +43,8 @@ export  const googleAuth = async(req, res) => {
     let user = await User.findOne({ email });
 
     if (!user) {
-      const normalizedRole = role === 'educator' || role === 'student' ? role : 'student';
+      const normalizedRole =
+        role === "educator" || role === "student" ? role : "student";
       user = await User.create({
         name,
         email,
@@ -49,34 +52,32 @@ export  const googleAuth = async(req, res) => {
         googleId,
         avatar,
       });
- }
+    }
 
-    // Generate Token 
+    // Generate Token
     const token = await genToken(user._id);
     if (!token) {
       return res.status(500).json({ message: "Token generation failed" });
     }
 
     // set cookie (optional) and return json
-    res.cookie('token', token, {
+    res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return res.json({
-      message:"Login Successfully",
+      message: "Login Successfully",
       token,
       user,
     });
-  }  catch (error) {
+  } catch (error) {
     console.log("Google auth error:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
-
-
 
 export const signup = async (req, res) => {
   try {
@@ -114,33 +115,34 @@ export const signup = async (req, res) => {
       password: hashPassword,
     });
     //generate tokken
-     const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     //setting tok(en) in cookie
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,      // required with sameSite: "none"
-    sameSite: "none",  // allow cross-site cookie
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // required with sameSite: "none"
+      sameSite: "none", // allow cross-site cookie
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
     //return res
-    return res
-      .status(201)
-      .json({ success: true, message: "User created ", user:{
-        name:user.name,
-        email:user.email,
-        role:user.role
-      } });
+    return res.status(201).json({
+      success: true,
+      message: "User created ",
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
     return res
       .status(500)
       .json({ success: false, message: `Sign up error ${error}` });
   }
 };
+
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -171,7 +173,7 @@ export const login = async (req, res) => {
     }
 
     // Match password
-    const isMatch =  bcrypt.compare(password, user.password);
+    const isMatch = bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({
         success: false,
@@ -180,33 +182,28 @@ export const login = async (req, res) => {
     }
 
     // Generate token
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
       secure: false, // true only in production
-      sameSite: 'lax',
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
-   
 
     // Response
     return res.status(200).json({
       success: true,
       message: "Login successful",
       user: {
-        email:user.email,
-        role:user.role,
-        user:user.name
+        email: user.email,
+        role: user.role,
+        user: user.name,
       },
     });
-
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -226,3 +223,101 @@ export const logout = async (req, res) => {
   }
 };
 
+export const sendOTP = async (req, res) => {
+  try {
+    //fetch email
+    const { email } = req.body;
+
+    //find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    //generate otp
+    const otp = Math.floor(1000 + Math.random() * 9000).toString();
+
+    //chnage in user
+    user.resetOtp = otp,
+    user.otpExpires = Date.now() + 5 * 60 * 100,
+    user.isOtpVerified = false,
+      //save user
+      await user.save();
+      //send otp
+    await sendMail(email, otp);
+
+    return res.status(200).json({ message: "Otp send successfully" });
+  } catch (error) {
+     return res
+      .status(500)
+      .json({ success: false, message: `send otp error ${error}` });
+  }
+};
+
+
+export const verifyOTP = async (req, res) => {
+  try {
+    //fetch email and otp
+    const { email , otp } = req.body;
+     //find user
+    const user = await User.findOne({ email });
+
+    //check otp is valid or not
+    if (!user || user.resetOtp != otp || user.otpExpires < Date.now()) {
+      return res.status(404).json({ message: "Invalid OTP" });
+    }
+
+      //change in user
+    user.isOtpVerified = true,
+    user.resetOtp = undefined,
+    user.otpExpires = undefined,
+    
+      //save user
+    await user.save();
+     
+
+    return res.status(200).json({ message: "Otp verified successfully" });
+  } catch (error) {
+     return res
+      .status(500)
+      .json({ success: false, message: `verify otp error ${error}` });
+  }
+};
+
+
+export const ResetPassword = async (req, res) => {
+  try {
+
+    //fetch email and password
+    const { email , password , confirmPassword } = req.body;
+
+     //find user
+    const user = await User.findOne({ email });
+
+    //check 
+    if (!user || !user.isOtpVerified) {
+      return res.status(404).json({ message: "OTP verification is required" });
+    }
+
+
+    //check 
+    if (password != confirmPassword) {
+      return res.status(404).json({ message: "Password are not matched" });
+    }
+
+    const hashPassword = await bcrypt.hash( password , 10);
+
+    user.password = hashPassword;
+    user.isOtpVerified = false;
+    
+    //save user
+    await user.save();
+     
+
+    return res.status(200).json({ message: "Password Reset successfully" });
+  } catch (error) {
+     return res
+      .status(500)
+      .json({ success: false, message: `Reset Password error ${error}` });
+  }
+};
