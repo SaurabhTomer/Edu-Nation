e6a22c9d6afc428b8389accb901a69aa4cecfc9a
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-17 02:25:18 +0530

    chnages in css

--- frontend/src/components/ReviewPage.jsx
+++ frontend/src/components/ReviewPage.jsx
@@ -12,9 +12,9 @@ function ReviewPage() {
     },[allReview])
   return (
      <div className='flex items-center justify-center flex-col'>
-      <h1 className='md:text-[45px] text-[30px] font-semibold text-center mt-[30px] px-[20px]'>Real Reviews from Real Learners</h1>
-      <span className='lg:w-[50%] md:w-[80%] text-[15px] text-center mt-[30px] mb-[30px] px-[20px]'>Discover how our Virtual Courses is transforming learning experiences through real feedback from students and professionals worldwide.</span>
-    <div className='w-[100%] min-[100vh] flex items-center justify-center flex-wrap gap-[50px] lg:p-[50px] md:p-[30px] p-[10px] mb-[40px]
+      <h1 className='md:text-[45px] text-[30px] font-semibold text-center mt-[30px] px-5'>Real Reviews from Real Learners</h1>
+      <span className='lg:w-[50%] md:w-[80%] text-[15px] text-center mt-[30px] mb-[30px] px-5'>Discover how our Virtual Courses is transforming learning experiences through real feedback from students and professionals worldwide.</span>
+    <div className='w-full min-[100vh] flex items-center justify-center flex-wrap gap-[50px] lg:p-[50px] md:p-[30px] p-2.5 mb-10
 
     '>
       
