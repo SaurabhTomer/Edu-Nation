2219bd5d00fc91ad5ed76424d41e80816aee2f69
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-16 21:07:05 +0530

    edit lecture controller

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -159,61 +159,96 @@ export const deleteCourse = async (req, res) => {
   }
 };
 
-
-export const createLecture = async (req , res) => {
+export const createLecture = async (req, res) => {
   try {
     //fetch data from body
-    const {lectureTitle} = req.body;
+    const { lectureTitle } = req.body;
     //fecth courseId from params
-    const {courseId} = req.params;
+    const { courseId } = req.params;
 
     //check
-    if( !lectureTitle || !courseId){
-      return res.status(400).json({message:"field is missing"})
+    if (!lectureTitle || !courseId) {
+      return res.status(400).json({ message: "field is missing" });
     }
 
     //crate Lecture
-    const lecture = await Lecture.create({lectureTitle});
+    const lecture = await Lecture.create({ lectureTitle });
     //find course by id
     const course = await Course.findById(courseId);
 
     // if course is present then push lecture in it
-    if(course){
+    if (course) {
       course.lectures.push(lecture._id);
     }
     //get all details of kecture like all (ispublished , titile , vidoeurl)
-     await course.populate("lectures");
-    //save course 
+    await course.populate("lectures");
+    //save course
     await course.save();
-      return res.status(201).json({ lecture , course });
-  
-
+    return res.status(201).json({ lecture, course });
   } catch (error) {
     return res.status(500).json({ message: "lecture create error", error });
   }
 };
 
-export const getCourseLecture = async (req , res) => {
+export const getCourseLecture = async (req, res) => {
   try {
     //fecth courseId from params
-    const {courseId} = req.params;
+    const { courseId } = req.params;
     //check
-    if( courseId){
-      return res.status(400).json({message:"course is required "})
+    if (!courseId) {
+      return res.status(400).json({ message: "course is required " });
     }
 
-  const course = await Course.findById({courseId});
-   if( course){
-      return res.status(404).json({message:"course is not found "})
+    const course = await Course.findById({ courseId });
+    if (course) {
+      return res.status(404).json({ message: "course is not found " });
     }
-//get all deatils of lecture
-    await  course.populate("lectures")
+    //get all deatils of lecture
+    await course.populate("lectures");
     await course.save();
-  
-      return res.status(200).json({ course });
-  
 
+    return res.status(200).json({ course });
   } catch (error) {
-    return res.status(500).json({ message: "get course lecture  error", error });
+    return res
+      .status(500)
+      .json({ message: "get course lecture  error", error });
   }
-}
+};
+
+export const editLecture = async (req, res) => {
+  try {
+    //fecth  from params
+    const { lectureId } = req.params;
+    const { isPreviewFree, lectureTitle } = req.body;
+
+    //find lecture by id
+    const lecture = await Lecture.findById({ lectureId });
+    //check
+    if (lecture) {
+      return res.status(404).json({ message: "Lecture is not found " });
+    }
+
+    // if video is given then uplaod it on cloudniary anf get url from it
+    let videoUrl;
+    if (req.file) {
+      videoUrl = await uploadOnCloudinary(req.file.path);
+      lecture.videoUrl = videoUrl;
+    }
+    //lecture title 
+    if (lectureTitle) {
+      lecture.lectureTitle = lectureTitle;
+    }
+    //is PreviewFree
+    lecture.isPreviewFree = isPreviewFree;
+
+
+    //save
+    await Lecture.save();
+
+    return res.status(200).json( lecture );
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: " lecture  edit  error", error });
+  }
+};
